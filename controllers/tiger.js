29efd11246ac2b2ef7753ab26014fbf3966b7f4f
srinivasmane09 <manesrinivas11@gmail.com>
2023-11-15 23:12:31 -0600

    commit

--- controllers/tiger.js
+++ controllers/tiger.js
@@ -138,6 +138,20 @@ res.status(500)
 res.send(`{'error': '${err}'}`);
 }
 };
+// Handle a delete one view with id from query
+exports.tiger_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await tiger.findById(req.query.id)
+res.render('tigerdelete', { title: 'tiger Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
 
 
     
--- routes/tiger.js
+++ routes/tiger.js
@@ -12,5 +12,6 @@ router.get('/', tiger_controlers.tiger_view_all_Page );
 router.get('/detail', tiger_controlers.tiger_view_one_Page);
 router.get('/create', tiger_controlers.tiger_create_Page);
 router.get('/update', tiger_controlers.tiger_update_Page);
+router.get('/delete', tiger_controlers.tiger_delete_Page);
 
 module.exports = router;
